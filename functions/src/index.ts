395dc7c347041a54535eb781063268d4e744bb9d
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-10-12 18:18:18 +0700

    initiate handlers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,15 @@
+import { HttpError, FirebaseContext } from 'tymon';
+import { loadEnv } from './utils/env';
+
+import Handlers from './handlers';
+
+/** load env */
+loadEnv();
+
+/** initiate singletons */
+HttpError.initialize();
+FirebaseContext.initialize({});
 
 export default {
-    
+    ...Handlers
 }
---
+++ functions/src/typings/common.d.ts
@@ -0,0 +1,36 @@
+declare module "*.json" {
+    const value: any;
+    export default value;
+}
+
+export interface DeterminerOut<Model> {
+    action: 'CREATE' | 'UPDATE' | 'DELETE',
+    data: Model
+}
+
+export interface IContext {
+    username: string;
+    user_id: string;
+}
+
+export interface IData {
+    query: any;
+    body: any;
+    method: string;
+    handler: string;
+}
+
+export interface IHttpError {
+    message: string;
+    name: string;
+    status: number;
+    data?: object;
+}
+
+export interface IHandlerOutput {
+    message?: string;
+    data?: any;
+    status?: number;
+}
+
+type methodHandler = (data: IData, context: IContext) => Promise<IHandlerOutput>;
